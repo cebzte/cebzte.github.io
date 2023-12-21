7601ab5415991e7b40ab1b3721b786d248c682b0
Alirezash2004 <alirezashabany2004@gmail.com>
2023-12-21 16:33:29 +0330

    fix nav size change isssue

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,13 @@
-let isScrolled = false;
-
 window.addEventListener('scroll', function () {
     let items = [document.querySelector('.nav-scroller'), document.querySelector('.blog-header-logo'), document.querySelector('nav'), document.querySelector('.uni-title')]
-    console.log(window.scrollY);
 
-    if (window.scrollY > 50 && !isScrolled) {
+    if (window.scrollY > 250) {
         items.forEach(item => {
             item.classList.add('active');
         });
-        isScrolled = true;
-    } else if (window.scrollY <= 10 && isScrolled) {
+    } else if (window.scrollY <= 100) {
         items.forEach(item => {
             item.classList.remove('active');
         });
-        isScrolled = false;
     }
 });
